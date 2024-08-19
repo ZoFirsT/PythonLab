ee617ce44c5a9a894dfd7d2345db48884fdecf0e
Thanatcha Saleekongchai <93702985+ZoFirsT@users.noreply.github.com>
2024-08-19 14:21:05 +0700

    Commit auto by ZoFirsT Bot

---
+++ 09.19_1-3/Lacture/ifelse.py
@@ -0,0 +1,8 @@
+a = 1
+if (a == 1):
+    print("a is 1")
+    a=a+10
+    print(a)
+
+print("a is not 1 but it", a)
+print("end of program")
---
+++ commit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Add all changes to the staging area
+git add .
+
+# Commit the changes with a message
+git commit -m "Commit auto by ZoFirsT Bot"
+
+# Push the changes to the remote repository
+git push
