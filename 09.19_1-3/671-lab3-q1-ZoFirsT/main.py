2223d57f93059c2ca69be2e00db4268aaf90d1c7
Thanatcha Saleekongchai <93702985+ZoFirsT@users.noreply.github.com>
2024-08-19 15:12:09 +0700

    Automated commit by ZoFirsT Bot: Mon Aug 19 15:12:09 +07 2024

---
+++ 09.19_1-3/671-lab3-q1-ZoFirsT/main.py
@@ -0,0 +1,9 @@
+hours_worked = float(input())
+
+if hours_worked <= 40:
+    compensation = hours_worked * 50
+else:
+    extra_hours = hours_worked - 40
+    compensation = (40 * 50) + (extra_hours * 75)
+
+print(compensation)
