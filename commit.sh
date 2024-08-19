3a3bbcb534e91b764f968ad9d1d861758774dbf8
Thanatcha Saleekongchai <93702985+ZoFirsT@users.noreply.github.com>
2024-08-19 14:22:10 +0700

    Commit auto by ZoFirsT Bot

--- commit.sh
+++ commit.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Add all changes to the staging area
 git add .
 
-# Commit the changes with a message
 git commit -m "Commit auto by ZoFirsT Bot"
 
-# Push the changes to the remote repository
 git push
