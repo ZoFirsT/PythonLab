0fded3c0a15fd3902f94bc2cf20ce7416299a8c6
Thanatcha Saleekongchai <93702985+ZoFirsT@users.noreply.github.com>
2024-09-16 17:15:10 +0700

    Automated commit by ZoFirsT Bot: Mon Sep 16 17:15:10 +07 2024

--- 09.16_1-7/671-lab7-q2-ZoFirsT/main.py
+++ 09.16_1-7/671-lab7-q2-ZoFirsT/main.py
@@ -1,12 +1,20 @@
-m = int(input())
-n = int(input())
+m, n = int(input()), int(input())
 
 matrix = []
-for _ in range(m):
-    row = [int(input()) for _ in range(n)]
+for i in range(m):
+    row = []
+    for j in range(n):
+        element = int(input())
+        row.append(element)
     matrix.append(row)
 
-zero_count = [sum(1 for row in matrix if row[col] == 0) for col in range(n)]
+zero_count = []
+for col in range(n):
+    count = 0
+    for row in range(m):
+        if matrix[row][col] == 0:
+            count += 1
+    zero_count.append(count)
 
 print(matrix)
 print(*zero_count, end=' ')
